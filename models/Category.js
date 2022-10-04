0284e3c9c39b34c3805ec79295fbfcb9e86f7e1a
Noahb26 <noahbrown2663@gmail.com>
2022-10-04 15:05:58 -0700

    add category model & index model

--- models/Category.js
+++ models/Category.js
@@ -4,10 +4,20 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// category model defined with id and category_name //
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNULL: false,
+      primaryKey: true,
+      autoIncrement: true
   },
+  category_name: { 
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+},
   {
     sequelize,
     timestamps: false,
--- models/index.js
+++ models/index.js
@@ -1,16 +1,30 @@
-// import models
+// import models //
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category //
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
+ 
+// Categories have many Products //
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+})
 
-// Categories have many Products
+// Products belongToMany Tags (through ProductTag) //
+Product.belongsToMany(Tag, {
+  through: {model: ProductTag, unique: false},
+  foreignKey: 'product_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) //
+Tag.belongsToMany(Product, {
+  through: {model: ProductTag, unique: false},
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
